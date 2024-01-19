393c7dda5eeba185db610241af8c2f21f9bf3e8d
lakhankag <157089762+lakhankag@users.noreply.github.com>
2024-01-19 10:24:00 +0000

    01

---
+++ 01_basics/01_variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail =" kaglekhraj@gmail.com"
+var accountPassword = "12345"
+accountCity = "indore"
+
+// accountId = 2; //not allowed
+
+accountEmail = "kag@gmail.com"
+accountPassword = "223232"
+accountCity = ' JABALPUR'
+
+console.log(accountId);
+
+console.table([accountId ,accountEmail,accountPassword, accountCity]);
